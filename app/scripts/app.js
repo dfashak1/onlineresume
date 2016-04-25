a44d6902b8750f69e676d2c690ec55e8dd5e0651
Cloud User <ec2-user@ip-172-31-31-203.us-west-2.compute.internal>
2016-04-25 19:32:53 -0400

    Initial commit of online reusme

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ayofashApp
+ * @description
+ * # ayofashApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('ayofashApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
+
+module.exports = app;
